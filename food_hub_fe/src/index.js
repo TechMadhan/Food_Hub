103ee6e6dd75b1627a2b3c3a37b75cf97e44feda
Stains M Johnson <grahamstains1999@gmail.com>
2022-06-11 17:10:34 +0530

    firebase signin

--- food_hub_fe/src/index.js
+++ food_hub_fe/src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import RootProvider from "./store/providers";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RootProvider>
+      <App />
+    </RootProvider>
   </React.StrictMode>
 );
 
